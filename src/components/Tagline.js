efa7e86f61e34ae7f35424eda35326bce2790fd6
Justin Duncan <floydfan_06@yahoo.com>
2022-10-16 18:56:28 -0700

    Source control fix

--- src/components/Tagline.js
+++ src/components/Tagline.js
@@ -8,24 +8,10 @@ const Tagline = () => {
     <div className="tagLine">
       <video src={ventPromo} loop autoPlay muted className="videoBackground" />
       <img src={fullLogo} alt="logo" className="logo d-none d-md-block" />
-<<<<<<< HEAD
       <h1 className="content">
           COMEDIANS
           <span className="unpack">UNPACK</span>
           <span className="gripes">YOUR GRIPES!</span>
-=======
-      <div className="contentContainer">
-      <Col className="content">
-        <h1 className="comedians">
-          COMEDIANS<br/>
-         
-          <span className="unpack">UNPACK</span>
-          
-          <br />
-          <span className="gripes">YOUR GRIPES!</span>
-          <br />
-          
->>>>>>> 9be311aabf107c944ec907defe7527209f77d991
           <a href="/#Shows">
             <Button
               className="button btn"
@@ -37,13 +23,7 @@ const Tagline = () => {
               Get Tickets!
             </Button>
           </a>
-<<<<<<< HEAD
         </h1>
-=======
-          </h1>
-        </Col>
-      </div>
->>>>>>> 9be311aabf107c944ec907defe7527209f77d991
       <hr />
     </div>
   );
