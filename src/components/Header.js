b9a568e65d236172261a437438fa5be8ca61608f
Justin Duncan <floydfan_06@yahoo.com>
2022-10-17 19:54:01 -0700

    Tagline formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,58 +1,66 @@
 import { useState } from "react";
-import { Navbar, Collapse, NavbarToggler, Nav, NavItem } from "reactstrap";
+import {
+  Navbar,
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  Container,
+} from "reactstrap";
 import { HashLink as NavLink } from "react-router-hash-link";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <Navbar className="navbar-dark" fixed="top" z-index="5" expand="md" >
-      <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
-      <Collapse isOpen={menuOpen} navbar>
-        <Nav sticky="top" className="ms-auto" navbar>
-          <NavItem>
-            <NavLink
-              className="nav-link"
-              to="/#Shows"
-              onClick={() => setMenuOpen(!menuOpen)}
-              style={{color: '#F16A74' }}
-              
-            >
-              Shows
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className="nav-link"
-              to="/#About"
-              onClick={() => setMenuOpen(!menuOpen)}
-              style={{color: '#F16A74'}}
-            >
-              About
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className="nav-link"
-              to="/#Vent"
-              onClick={() => setMenuOpen(!menuOpen)}
-              style={{color: '#F16A74'}}
-            >
-              Vent!
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className="nav-link"
-              to="/#Contact"
-              onClick={() => setMenuOpen(!menuOpen)}
-              style={{color: '#F16A74'}}
-            >
-              Contact
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
+    <div className="navContainer">
+      <Navbar className="navbar-dark" fixed="top" z-index="5" expand="md">
+        <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+        <Collapse isOpen={menuOpen} navbar>
+          <Nav sticky="top" className="ms-auto" navbar>
+            <NavItem>
+              <NavLink
+                className="nav-link"
+                to="/#Shows"
+                onClick={() => setMenuOpen(!menuOpen)}
+                style={{ color: "#F16A74" }}
+              >
+                Shows
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className="nav-link"
+                to="/#About"
+                onClick={() => setMenuOpen(!menuOpen)}
+                style={{ color: "#F16A74" }}
+              >
+                About
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className="nav-link"
+                to="/#Vent"
+                onClick={() => setMenuOpen(!menuOpen)}
+                style={{ color: "#F16A74" }}
+              >
+                Vent!
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className="nav-link"
+                to="/#Contact"
+                onClick={() => setMenuOpen(!menuOpen)}
+                style={{ color: "#F16A74" }}
+              >
+                Contact
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
   );
 };
 
--- src/components/Tagline.js
+++ src/components/Tagline.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Row, Col, CardImgOverlay } from "reactstrap";
+import { Button } from "reactstrap";
 import fullLogo from "../app/assets/img/fullLogo.png";
 import ventPromo from "../app/assets/ventPromo.mp4";
 
@@ -7,9 +7,7 @@ const Tagline = () => {
   return (
     <div className="tagLine">
       <video src={ventPromo} loop autoPlay muted className="videoBackground" />
-      <img src={fullLogo} alt="logo" className="logo d-none d-md-block" />
-      
-        
+      <img src={fullLogo} alt="logo" className="logo d-none d-sm-block" />        
           <h1 className="content">
             COMEDIANS
             <br />
